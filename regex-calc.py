64a02316bf44db1efa0950ca51b8b25e3abfcb11
Augusto <ammguto@gmail.com>
2025-02-17 14:24:02 -0300

    [Prioridade] Adicionada prioridade por parênteses

--- regex-calc.py
+++ regex-calc.py
@@ -9,6 +9,7 @@ def busca_operacao(text):
     while(1):
         num1 = 0
         num2 = 0
+        numRes = re.search(numPatt, text)
         sumRes = re.search(sumPatt, text)
         subRes = re.search(subPatt, text)
         multRes = re.search(multPatt, text)
@@ -16,7 +17,9 @@ def busca_operacao(text):
         if(divRes):
             num1 = float(divRes.group(1))
             num2 = float(divRes.group(3))
-            if(num2 == 0): print("Não é possível dividir por 0")
+            if(num2 == 0):
+                print("Não é possível dividir por 0")
+                return 0
             else: 
                 accumulator = num1 / num2
                 text = text.replace(divRes.group(), str(accumulator))
@@ -38,23 +41,38 @@ def busca_operacao(text):
             accumulator = num1 - num2
             text = text.replace(subRes.group(), str(accumulator))
             if(accumulator == float(text)): break
+        elif(numRes):
+            accumulator = float(numRes.group())
+            break
         else: break
     return accumulator
 
+def busca_parenteses(text):
+    parentRes = re.search(parentPatt, text)
+    if(parentRes):
+        # Substituindo o resultado da operação dentro do parênteses pelo match do par de parênteses
+        text = text.replace(parentRes.group(), str(busca_operacao(parentRes.group(0))))
+        return busca_parenteses(text)
+    else:
+        return busca_operacao(text)
+    
+#nenhum ou mais dígito "." nenhum ou mais dígito
+numPatt = r"\d+\.?\d*"
 #nenhum ou mais dígito "+" Pelo menos 1 dígito
-sumPatt = r"(\d*\.?\d*)(\s*\+\s*)(\d*\.?\d*)"
+sumPatt = r"(\d*\.?\d*)(\s*\+\s*)(\d+\.?\d*)"
 #nenhum ou mais dígito "-" Pelo menos 1 dígito
-subPatt = r"(\d*\.?\d*)(\s*\-\s*)(\d*\.?\d*)"
+subPatt = r"(\d*\.?\d*)(\s*\-\s*)(\d+\.?\d*)"
 # Pelo menos um dígito "*" Pelo menos 1 dígito
-multPatt = r"(\d*\.?\d*)(\s*\*\s*)(\d*\.?\d*)"
+multPatt = r"(\d+\.?\d*)(\s*\*\s*)(\d+\.?\d*)"
 # Pelo menos 1 Dígito "/" Pelo menos 1 Dígito != 0
-divPatt = r"(\d*\.?\d*)(\s*\/\s*)(\d*\.?\d*)"
+divPatt = r"(\d+\.?\d*)(\s*\/\s*)(\d+\.?\d*)"
+# Par de parênteses
+parentPatt  = r"\([^\(\)]*\)"
 
-accumulator = 0
 text = ""
-while (text != "\3"):
+while (text != '\3'):
     text = input("")
-    print(f"Resultado: {busca_operacao(text)}")
+    print(f"Resultado: {busca_parenteses(text)}")
 
 
 # "3 + 5" 	8
@@ -62,13 +80,3 @@ while (text != "\3"):
 # "4 + 8 / 2" 	8
 # "2.5 * 4 - 1" 	9
 # "(3 + 5) * 2" 	16 (se implementar parênteses)
-
-# Exemplo
-# text = "Meu número de telefone é 123-456-7890"
-# padrao = r"\d{3}-\d{3}-\d{4}"
-
-# resultado = re.search(padrao, text)
-# if resultado:
-#     print("Encontrado:", resultado.group())
-# else:
-#     print("Não encontrado")
